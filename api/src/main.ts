e5e6a76aad16f32074fb12ce2d940c9d9eec4a06
Ivan Ulloque <95107566+ZUR1C4T0@users.noreply.github.com>
2023-05-23 15:56:39 +0000

    feat(api): :heavy_plus_sign: added dependencies and validation configuration

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  // This is the validation pipe that will be used globally
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  )
+
   await app.listen(3000)
 }
 bootstrap()
